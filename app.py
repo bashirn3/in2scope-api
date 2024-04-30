43c5812071d133ea1782329a65c8ef7c3bd41258
Bashir Sani <bashir.sani@flexisaf.com>
2024-04-30 22:02:53 +0100

    added server host]

--- app.py
+++ app.py
@@ -335,7 +335,7 @@ def filter_schools2(schools, candidate_postcode, max_travel_time, mode_of_transp
 
 
 if __name__ == '__main__':
-    app.run(debug=True,port=os.getenv('PORT'))
+    app.run(debug=True,host='0.0.0.0',port=os.getenv('PORT'))
 
 
 
