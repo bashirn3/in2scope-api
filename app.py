1012be7d817d0f92f2b36b7c6c9bf162231de69d
Bashir Sani <bashir.sani@flexisaf.com>
2024-08-07 13:30:18 +0100

    added stuff render

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ from flask_cors import CORS
 
 from dotenv import load_dotenv
 
-
-
 app = Flask(__name__)
 CORS(app)
 
@@ -21,16 +19,15 @@ CORS(app)
 load_dotenv()
 
 # Google Maps API key
-GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
+GOOGLE_MAPS_API_KEY = "AIzaSyCXe_or92ZvVfjUGcb5q3CrkUlFfp2FAOY"
 
 
-# Establish a connection to the PostgreSQL database
-# "dpg-cm0k1ei1hbls73dbibjg-a.oregon-postgres.render.com
-conn = psycopg2.connect(
-    host="dpg-conr3u21hbls73fp2h10-a.oregon-postgres.render.com",
-    database="in2scope2_itk7",
-    user="in2scopeuser",
-    password="1dUd7javDFVD6QJ6YhMRsqRihgnzvvhR"
+
+conn = psycopg2.connect (
+    host = "dpg-cqpm3otsvqrc73fu9pf0-a.oregon-postgres.render.com",
+    database = "eduscope",
+    user="eduscopeuser",
+    password="TsUgJQBQg3Up35tiJkexxwfZzZgeLhIr"
 )
 
 # Open a cursor to perform database operations
@@ -48,6 +45,7 @@ def find_schools():
     try:
         # Get data from the request
         data = request.get_json()
+        borough = request.args.get('borough')  # Access borough from query parameter
 
         # Validate required fields
         required_fields = ['postcode', 'mode_of_transport', 'max_travel_time']
@@ -65,7 +63,7 @@ def find_schools():
         mode_of_transport = data['mode_of_transport']
 
         # Retrieve schools from the database
-        cur.execute('SELECT * FROM schools;')
+        cur.execute("SELECT * FROM schools WHERE borough = %s;", (borough,))
 
         schools = cur.fetchall()
 
@@ -78,6 +76,52 @@ def find_schools():
         return jsonify({'error': str(ve)}), 400  # Bad Request
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+
+    
+@app.route('/find_schools/<id>', methods=['GET'])
+def get_school_by_id(id):
+    try:
+
+        # Execute the SQL query to retrieve the school and its contact information
+        query = """
+        SELECT 
+        schools.schoolname,
+        contacts.first_name, 
+        contacts.last_name, 
+        contacts.email, 
+        contacts.position
+
+        FROM schools 
+        RIGHT JOIN contacts ON contacts.idd = schools.schoolname 
+        WHERE schools.id = %s;
+        """
+        cur.execute(query, (id,))
+
+        schools = cur.fetchall()
+
+        if not schools:
+            return jsonify({'error': 'School not found'}), 404  # Not Found
+        
+        contacts = []
+        for row in schools:
+            contact = {
+                'school_name': row[0],
+                'first_name': row[1],
+                'last_name': row[2],
+                'email': row[3],
+                'position': row[4]
+            }
+            contacts.append(contact)
+
+        return jsonify({'contacts': contacts})
+
+
+    except ValueError as ve:
+        return jsonify({'error': str(ve)}), 400  # Bad Request
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500  # Internal Server Error
+
     
 
 
@@ -122,50 +166,9 @@ def filter_csv():
         mode_of_transport = request.form.get('mode_of_transport')
 
 
-    
-       
-    
-        #     # Validate required fields
-        # required_fields = ['postcode', 'mode_of_transport', 'max_travel_time']
-        # for field in required_fields:
-        #         if field not in data:
-        #             raise ValueError(f"Missing required field: {field}")
-
-        # validate_postcode(data['postcode'])
-        # validate_mode_of_transport(data['mode_of_transport'])
-        # validate_max_travel_time(data['max_travel_time'])
-
-        #     # Extract candidate's information
-        #     candidate_postcode = data['postcode']
-        #     max_travel_time = data['max_travel_time']
-        #     mode_of_transport = data['mode_of_transport']
-        # else:
-        #  raise ValueError("Missing JSON data in the request.")
-
-        filtered_schools = filter_schools2(schools, candidate_postcode, max_travel_time, mode_of_transport)
+        filtered_schools = filter_schools_parallel(schools, candidate_postcode, max_travel_time, mode_of_transport)
         return jsonify(filtered_schools)
 
-
-        # return jsonify(schools)
-
-        
-        # Filter schools based on the CSV data
-       
-
-      
-
-        # # Create a new CSV in memory
-        # output_csv = StringIO()
-        # csv_writer = csv.DictWriter(output_csv, fieldnames=['postcode', 'First Name', 'LastName', 'Email', 'Name' 'travel_time'])
-        # csv_writer.writeheader()
-        # csv_writer.writerows(filtered_schools)
-
-        # # Return the new CSV as a downloadable file
-        # output_csv.seek(0)
-        # return send_file(BytesIO(output_csv.read()),
-        #                  mimetype='text/csv',
-        #                  download_name='filtered_results.csv',
-        #                  as_attachment=True)
 
     except ValueError as ve:
         return jsonify({'error': str(ve)}), 400  # Bad Request
@@ -176,18 +179,10 @@ def filter_csv():
 
 
 
-
-
-
-
-
-
-
 # helper functions
 
 def validate_postcode(postcode):
-    # Add your postcode validation logic here
-    # For example, check if the postcode format is valid
+    # check if the postcode format is valid
     if not postcode or not isinstance(postcode, str) or len(postcode) < 5:
         raise ValueError("Invalid postcode")
 
@@ -218,52 +213,6 @@ def get_travel_time(origin_postcode, destination_postcode, mode_of_transport):
     except (KeyError, IndexError):
         # Handle invalid response or missing data
         return float('inf')  # Return infinity for an invalid travel time
-
-# def get_travel_time(origin, destinations, mode_of_transport):
-#     # Check cache first
-#     cache_key = (origin, tuple(destinations), mode_of_transport)
-  
- 
-#     if cache_key in travel_time_cache:
-#         return travel_time_cache[cache_key]
-    
-
-
-#     # Call Google Maps Distance Matrix API for batch processing
-#     matrix = gmaps.distance_matrix(origin, destinations, mode=mode_of_transport)
-
-
-#     # Parse results and extract travel time
-#     travel_times = []
-#     for element in matrix['rows'][0]['elements']:
-#         if 'duration' in element:
-#             travel_times.append(element['duration']['value'])
-#         else:
-#             # Handle cases where travel time couldn't be calculated
-#             travel_times.append(float('inf'))
-
-#     # Update cache
-#     travel_time_cache[cache_key] = travel_times
-
-   
-#     return travel_times
-
-# def get_travel_time(origin, destinations, mode_of_transport):
-#     # Call Google Maps Distance Matrix API for batch processing
-#     matrix = gmaps.distance_matrix(origin, destinations, mode=mode_of_transport)
-
-#     # Parse results and extract travel time
-#     travel_times = []
-#     for element in matrix['rows'][0]['elements']:
-#         if 'duration' in element:
-#             travel_times.append(element['duration']['value'])
-#         else:
-#             # Handle cases where travel time couldn't be calculated
-#             travel_times.append(float('inf'))
-
-#     return travel_times
-
-
 
 
 
@@ -301,41 +250,10 @@ def filter_schools_parallel(schools, candidate_postcode, max_travel_time, mode_o
 
     return filtered_schools
 
-
-
-
-def filter_schools2(schools, candidate_postcode, max_travel_time, mode_of_transport):
-    filtered_schools=[]
-    for school in schools:
-        # Get travel time from Google Maps Distance Matrix API
-        travel_time = get_travel_time(candidate_postcode, school["postcode"], mode_of_transport)
-
-        max_travel_time = float(max_travel_time)
-
-        # Check if the school is within the specified max travel time
-        # postcode', 'First Name', 'Last Name', 'Email', 'Name' "School Name"
-        if travel_time <= max_travel_time:
-            filtered_schools.append({
-                'schoolname': school["Name"],
-                'postcode': school["postcode"],
-                'borough': school["Borough"],
-                'email': school["Email"],
-                'first name': school["First Name"],
-                'last name': school["Last Name"],
-                'position': school["Postion"],
-                'travel_time': int(travel_time)
-            })
-    
-
-
-    # Sort schools based on travel time
-    filtered_schools.sort(key=lambda x: x['travel_time'])
-
-    return filtered_schools
-
+            
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0', port="3999")
 
 
 
